5b10a1971c91d6c5a517ab839a8e70e97f296216
DavidSant021 <146587018+DavidSant021@users.noreply.github.com>
2024-04-08 22:56:58 -0300

    adicionada footer

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,18 +1,22 @@
 import { Outlet } from "react-router-dom";
 import AddTransaction from "../components/AddTransaction";
 import ContentCards from "../components/ContentCards";
+import { FaDollarSign } from "react-icons/fa6";
 
 export default function RootLayout() {
     return (
         <>
           <header className="header">
-            <h1>My Finances</h1>
+            <h1>My Finances<FaDollarSign style={{color: '#49AA26'}} /> </h1>
           </header>
           <main className="main">
             <ContentCards />
             <AddTransaction />
             <Outlet />
           </main>
+          <footer className="footer">
+            <p>My Finances<FaDollarSign style={{color: '#49AA26'}} /> </p>
+          </footer>
         </>
       )
 }
